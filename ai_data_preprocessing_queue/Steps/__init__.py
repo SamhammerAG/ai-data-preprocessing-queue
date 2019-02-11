126191a6f657d923d77b143a6a7ae5a64a95f9e9
Stefan Rom <srom@samhammer.de>
2019-02-11 20:55:22 +0100

    * Automatically import all modules inside steps directory

---
+++ ai_data_preprocessing_queue/Steps/__init__.py
@@ -0,0 +1,7 @@
+from . import *
+from os.path import dirname, basename, isfile
+from glob import glob
+
+# Loads all modules that are inside this directory
+modules = glob(dirname(__file__)+"/*.py")
+__all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
