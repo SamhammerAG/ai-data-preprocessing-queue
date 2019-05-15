bd84f9b53badd1cebba6c459783a88218aed2e77
gabbersepp <gabbersepp@googlemail.com>
2019-05-15 17:03:48 +0200

    fix: ignore lines with more or less than 3 columns and ignore columns where third column is not a number

--- ai_data_preprocessing_queue/Steps/token_replacement.py
+++ ai_data_preprocessing_queue/Steps/token_replacement.py
@@ -7,8 +7,9 @@ import re
 # the higher the number the higher the prio
 def step(item: str, itemState: dict, globalState: dict, preprocessorData: str) -> str:
 
-    lines = [[s.strip() for s in l.split(",")] for l in preprocessorData.splitlines()]
-    
+    lines = [[s.strip() for i, s in enumerate(l.split(",")) if (i == 2 and re.compile(r"^[0-9\s]+$").match(s)) or i < 2 ] for l in preprocessorData.splitlines() if l.count(",") == 2]
+    lines = [l for l in lines if len(l) == 3]
+
     i = 0
     while i < len(lines):
         lines[i][2] = int(lines[i][2])
