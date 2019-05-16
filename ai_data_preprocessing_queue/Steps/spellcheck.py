0cfce325217352d89f8c464bdccf879cbb040339
Thomas Franke <t.franke90@web.de>
2019-05-16 16:24:53 +0200

    AID-14270 code cleanup

--- ai_data_preprocessing_queue/Steps/spellcheck.py
+++ ai_data_preprocessing_queue/Steps/spellcheck.py
@@ -1,6 +1,7 @@
 import numpy as np
 from functools import reduce
 
+
 def step(item: str, itemState, globalState, preprocessorData: str):
     if preprocessorData == None:
         return item
@@ -12,12 +13,12 @@ def step(item: str, itemState, globalState, preprocessorData: str):
 
     values = set([len(w) for w in words])
     groupedReplaceWords = [{"key": key, "items": list(filter(lambda x: len(x) == key, words))} for key in values]
-    allItemWords = set(item.split(" ")) # reduce all words
+    allItemWords = set(item.split(" "))  # reduce all words
     # all words with more than 4 can have distance 2, al other 1
 
     for itemWord in allItemWords:
         length = len(itemWord)
-        allWordsToCheck = reduce(lambda x,y: x + y, [x.get("items") for x in groupedReplaceWords if x.get("key") >= length-2 and x.get("key") <= length+2]) 
+        allWordsToCheck = reduce(lambda x, y: x + y, [x.get("items") for x in groupedReplaceWords if length - 2 <= x.get("key") <= length + 2])
 
         for w in allWordsToCheck:
             if len(itemWord) < 4 and levenshtein(itemWord, w) == 1:
@@ -27,28 +28,29 @@ def step(item: str, itemState, globalState, preprocessorData: str):
 
     return item
 
+
 def levenshtein(seq1, seq2):
-   size_x = len(seq1) + 1
-   size_y = len(seq2) + 1
-   matrix = np.zeros ((size_x, size_y))
-   for x in range(size_x):
-       matrix [x, 0] = x
-   for y in range(size_y):
-       matrix [0, y] = y
+    size_x = len(seq1) + 1
+    size_y = len(seq2) + 1
+    matrix = np.zeros((size_x, size_y))
+    for x in range(size_x):
+        matrix[x, 0] = x
+    for y in range(size_y):
+        matrix[0, y] = y
 
-   for x in range(1, size_x):
-       for y in range(1, size_y):
-           if seq1[x-1] == seq2[y-1]:
-               matrix [x,y] = min(
-                   matrix[x-1, y] + 1,
-                   matrix[x-1, y-1],
-                   matrix[x, y-1] + 1
-               )
-           else:
-               matrix [x,y] = min(
-                   matrix[x-1,y] + 1,
-                   matrix[x-1,y-1] + 1,
-                   matrix[x,y-1] + 1
-               )
+    for x in range(1, size_x):
+        for y in range(1, size_y):
+            if seq1[x-1] == seq2[y-1]:
+                matrix[x, y] = min(
+                    matrix[x-1, y] + 1,
+                    matrix[x-1, y-1],
+                    matrix[x, y-1] + 1
+                )
+            else:
+                matrix[x, y] = min(
+                    matrix[x-1, y] + 1,
+                    matrix[x-1, y-1] + 1,
+                    matrix[x, y-1] + 1
+                )
 
-   return (matrix[size_x - 1, size_y - 1])
+    return matrix[size_x - 1, size_y - 1]
