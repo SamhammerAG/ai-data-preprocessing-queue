a136d506824ec046d34402793c0e186ce3120f87
Thomas Franke <t.franke90@web.de>
2019-05-21 16:17:11 +0200

    AID-14354 remove redundant list

--- ai_data_preprocessing_queue/Pipeline.py
+++ ai_data_preprocessing_queue/Pipeline.py
@@ -1,13 +1,13 @@
 from .StepProcessor import StepProcessor
-from typing import Dict
+from typing import Dict, Optional
 
 
 class Pipeline:
 
-    def __init__(self, step_names, preprocessorData: Dict[str, str] = {}):
+    def __init__(self, step_dict: Dict[str, Optional[str]] = {}):
         self.step_processors = []
-        for step_name in list(filter(None, step_names)):
-            processor = StepProcessor(step_name, preprocessorData.get(step_name))
+        for step_name in list(filter(None, step_dict.keys())):
+            processor = StepProcessor(step_name, step_dict.get(step_name))
             self.step_processors.append(processor)
 
     def consume(self, text, globalState=None):
