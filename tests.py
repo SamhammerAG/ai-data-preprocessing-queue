9ffcee6c4e5bd10cdd516ce6f69f6b6f05906e9b
Thomas Franke <t.franke90@web.de>
2019-05-21 13:33:34 +0200

    AID-14354 update and add unit tests for recent changes

--- tests.py
+++ tests.py
@@ -30,8 +30,15 @@ class PipelineTest(unittest.TestCase):
         value = pipeline.consume('123 CamelCase')
         self.assertEqual('    camelcase', value)
 
-    def test_number_interpretation(self):
+    def test_number_interpretation_do_not_crash_for_no_data(self):
         pipeline = Pipeline(["number_interpretation"])
+        value = pipeline.consume("test text")
+        self.assertEqual("test text", value)
+
+    def test_number_interpretation(self):
+        handler = open("./number_interpretation_testdata.csv", "r")
+        pipeline = Pipeline(["number_interpretation"], {"number_interpretation": handler.read()})
+        handler.close()
         # date
         value = pipeline.consume("test 1.1.2019 20.2.2003 1.1.20 01.01.20 1.1.1900 1.1. 01.01. test")
         self.assertEqual('test  replaceddate   replaceddate   replaceddate  replaceddate replaceddate   replaceddate  replaceddate test', value)
@@ -52,28 +59,10 @@ class PipelineTest(unittest.TestCase):
         value = pipeline.consume("test 015125391111 test")
         self.assertEqual('test  replacedgermanphonenumber  test', value)
 
-    def test_number_interpretation_with_preprocessor_data(self):
-        handler = open("./number_interpretation_testdata.csv", "r")
-        pipeline = Pipeline(["number_interpretation"], {"number_interpretation": handler.read()})
-        # should not replace dates with custom data
-        value = pipeline.consume("test 1.1.2019 20.2.2003 1.1.20 01.01.20 1.1.1900 1.1. 01.01. test")
-        self.assertEqual('test 1.1.2019 20.2.2003 1.1.20 01.01.20 1.1.1900 1.1. 01.01. test', value)
-        # iban
-        value = pipeline.consume("test DE12500101170648489890")
-        self.assertEqual('test  replacediban ', value)
-        # postcode
-        value = pipeline.consume("test 92637 92709 test")
-        self.assertEqual('test  replacedpostcode   replacedpostcode  test', value)
-        # german phone
-        value = pipeline.consume("test 0961123456 test")
-        self.assertEqual('test  replacedgermanphonenumber  test', value)
-        value = pipeline.consume("test (0961)123456 test")
-        self.assertEqual('test  replacedgermanphonenumber  test', value)
-        value = pipeline.consume("test +49(0)121-79536-77 test")
-        self.assertEqual('test  replacedgermanphonenumber  test', value)
-        # german handy
-        value = pipeline.consume("test 015125391111 test")
-        self.assertEqual('test  replacedgermanphonenumber  test', value)
+    def test_token_replacement_do_not_crash_for_no_data(self):
+        pipeline = Pipeline(["token_replacement"])
+        value = pipeline.consume("test text")
+        self.assertEqual("test text", value)
 
     def test_token_replacement(self):
         handler = open("./token_replacement_testdata.csv", "r")
@@ -87,10 +76,17 @@ class PipelineTest(unittest.TestCase):
         pipeline = Pipeline(["token_replacement"], {"token_replacement": handler.read()})
         handler.close()
         value = pipeline.consume("test abg. abgabgeschlossen 1212")
-        self.assertEqual('test abgeschlossen. abgabgeschlossen 1212', value)
+        self.assertEqual('test abgeschlossen abgabgeschlossen 1212', value)
+
+    def test_token_replacement_also_replace_dots_at_end_of_phrase(self):
+        handler = open("./token_replacement_testdata.csv", "r")
+        pipeline = Pipeline(["token_replacement"], {"token_replacement": handler.read()})
+        handler.close()
+        value = pipeline.consume("abg. 1212")
+        self.assertEqual('abgeschlossen 1212', value)
 
     def test_spellcheck(self):
-        pipeline = Pipeline(["spellcheck"], { "spellcheck": "kopie\r\nartikel\r\n"})
+        pipeline = Pipeline(["spellcheck"], {"spellcheck": "kopie\r\nartikel\r\n"})
         value = pipeline.consume("kopie koipe artikel artikle artilek artleki")
         self.assertEqual('kopie kopie artikel artikel artikel artleki', value)
 
