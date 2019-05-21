c6111a9d1258d46ff54528fafb25a282df926600
Thomas Franke <t.franke90@web.de>
2019-05-21 13:29:31 +0200

    AID-14354 number interpretation: cache preprocessor data to global state, if available and only load transmitted preprocessor data

--- ai_data_preprocessing_queue/Steps/number_interpretation.py
+++ ai_data_preprocessing_queue/Steps/number_interpretation.py
@@ -1,16 +1,36 @@
 import re
-import os
 from io import StringIO
-from os.path import dirname
 import pandas
 
 
-def step(item, itemState, globalState, preprocessorData: str):
-    if preprocessorData is "":
-        csvPath = os.path.join(dirname(__file__), "..", "data", "number_interpretation.csv")
-        csv = pandas.read_csv(csvPath, header=None, usecols=[0, 1, 2], quotechar='"', encoding="utf8")
+def step(item: str, itemState: dict, globalState: dict, preprocessorData: str):
+    if preprocessorData is None or preprocessorData == "":
+        return item
+
+    csv = _get_data_from_store_or_reload(globalState, preprocessorData)
+
+    for _, row in csv.iterrows():
+        pattern = re.compile(row[0])
+        item = pattern.sub(' ' + row[1] + ' ', item)
+
+    return item
+
+
+def _get_data_from_store_or_reload(globalState: dict, preprocessorData: str) -> pandas.DataFrame:
+    if globalState is not None:
+        dictIdentifier = "numberInterpretationPreprocessorData"
+        if dictIdentifier not in globalState:
+            preparedData = _prepare_pre_processor_data(preprocessorData)
+            globalState[dictIdentifier] = preparedData
+            return preparedData
+        else:
+            return globalState[dictIdentifier]
     else:
-        csv = pandas.read_csv(StringIO(preprocessorData), header=None, usecols=[0, 1, 2], quotechar='"', encoding="utf8")
+        return _prepare_pre_processor_data(preprocessorData)
+
+
+def _prepare_pre_processor_data(preprocessorData: str) -> pandas.DataFrame:
+    csv = pandas.read_csv(StringIO(preprocessorData), header=None, usecols=[0, 1, 2], quotechar='"', encoding="utf8")
 
     csv[0] = csv[0].str.strip()
     csv[1] = csv[1].str.strip()
@@ -18,10 +38,4 @@ def step(item, itemState, globalState, preprocessorData: str):
     csv["sort"] = csv[2]
 
     # sort
-    csv = csv.sort_values("sort", inplace=False).drop("sort", "columns")
-
-    for _, row in csv.iterrows():
-        pattern = re.compile(row[0])
-        item = pattern.sub(' ' + row[1] + ' ', item)
-
-    return item
+    return csv.sort_values("sort", inplace=False).drop("sort", "columns")
