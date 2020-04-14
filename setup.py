1294a995c3bb6b4924ecdd791de1499f5963157b
Stefan Rom <srom@samhammer.de>
2020-04-14 12:28:53 +0200

    Updated to latest versions of libraries

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open("README.md", "r") as fh:
 
 setup(
     name="ai-data-preprocessing-queue",
-    version="1.0.0",
+    version="1.1.0",
     description="Can be used to pre process data before ai processing",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/SamhammerAG/ai-data-preprocessing-queue",
     packages=find_packages(exclude=["test_data"]),
-    install_requires=['langdetect==1.0.7', 'nltk==3.4', "pandas==0.24.2", "numpy==1.16"]
+    install_requires=['langdetect==1.0.8', 'nltk==3.5', "pandas==1.0.3", "numpy==1.18.2"]
 )
